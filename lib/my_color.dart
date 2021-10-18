b35e2d0bd374969c70178fdaf778bbe41847991b
saci zakaria <sacideveloppeur@gmail.com>
2021-10-18 08:45:24 +0200

    updateShouldNotify

---
+++ lib/my_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyColor extends InheritedWidget{
+
+
+  @override
+  bool updateShouldNotify(covariant MyColor oldWidget) {
+    // TODO: implement updateShouldNotify
+    throw UnimplementedError();
+  }
+
+
+}
